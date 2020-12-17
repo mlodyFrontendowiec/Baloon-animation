32af116b93b2b63fb05e5c6428192531c5929119
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2020-12-17 20:45:38 +0100

    Create Animbg class project

---
+++ Animbg.js
@@ -0,0 +1,25 @@
+class Animbg {
+  constructor() {
+    this.elements = document.querySelectorAll(".anim-bg");
+  }
+  listenCursorMove(event) {
+    const { clientX, clientY } = event;
+    this.handleAnimation(clientX, clientY);
+  }
+  handleAnimation(clientX, clientY) {
+    this.elements.forEach((element) => {
+      const ratioX = -element.getAttribute("ratioX");
+      const ratioY = -element.getAttribute("ratioY");
+
+      const centerX = window.innerWidth / 2;
+      const centerY = window.innerHeight / 2;
+
+      const moveX = clientX - centerX;
+      const moveY = clientY - centerY;
+
+      element.style.transform = `translate(${moveX * ratioX}px,${
+        moveY * ratioY
+      }px)`;
+    });
+  }
+}
