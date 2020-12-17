8347c63c05c8e4be88635696d7afafe201ab8582
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2020-12-17 21:57:49 +0100

    last verison

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const elements = document.querySelectorAll(".anim-bg");
   animBg = new Animbg();
   document.addEventListener("mousemove", animBg.listenCursorMove);
 });
